60f8204415c87ee85bda2489d9ef95cdbe406fac
HAL22 <thethelafaltein@gmail.com>
2018-07-11 15:21:01 +0200

    WASD movement implemented, but rotation still a problem

---
+++ 3dShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public float speed = 10f;
+
+    private Transform player;
+
+
+    void Start ()
+    {
+
+        player = GetComponent<Transform>();
+
+
+	}
+
+    void rotate()
+    {
+
+        
+
+
+           Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+           float midpoint = (transform.position - Camera.main.transform.position).magnitude*0.5f;
+
+           transform.LookAt(mouseRay.origin + mouseRay.direction * midpoint);
+
+
+    }
+
+    void movement()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        player.Translate(speed*moveHorizontal*Time.deltaTime,0f,speed*moveVertical*Time.deltaTime);
+
+        rotate();
+
+    }
+	
+	
+	void Update ()
+    {
+
+        movement();
+
+       
+
+    }
+}
