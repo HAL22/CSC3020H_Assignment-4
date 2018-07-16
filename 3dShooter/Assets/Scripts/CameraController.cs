ae8962bb05ac43799bf73fd6c62befb8d5abebdd
HAL22 <thethelafaltein@gmail.com>
2018-07-16 16:35:14 +0200

    Still need to orbit camera properly

--- 3dShooter/Assets/Scripts/CameraController.cs
+++ 3dShooter/Assets/Scripts/CameraController.cs
@@ -5,21 +5,52 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
 
+    // Public variables
     public GameObject player;
+    public float radx=0;
+    public float rady=0;
+    public float radz=0;
+    public bool rot = true;
+   
 
+    // Private variables
     private Vector3 offset;
 
     // Use this for initialization
-    void Start () {
-
+    void Start ()
+    {
+        // The initial offset;
         offset = transform.position - player.transform.position;
-
     }
 	
 	// Update is called once per frame
-	void LateUpdate () {
+	void LateUpdate ()
+    {
 
-        transform.position = player.transform.position + offset;
+
+
+
+        
+
+        // making sure the camera looks at the player object
+
+        transform.LookAt(player.transform);
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+
+            transform.RotateAround(player.transform.position, Vector3.up, Time.deltaTime * 100);
+
+        }
+
+       // transform.position = player.transform.position + offset;
+
+
+        
+
+
+
+
 
     }
 }
